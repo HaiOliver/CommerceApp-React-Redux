d7e73095a9cbf343403feeb1c891f6ac605bcbd2
Oliver Huynh <haioliver993@gmail.com>
2020-07-01 12:52:43 -0700

    fixing Stripe bug

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
 
 const StripeCheckoutButton = ({price}) => {
     // set price on Strive
     const priceForStripe = price * 100;
     //get publish key from dashboard
     const publishableKey = 'pk_test_51GyQSmAylIb4YvR9zLMOBDeUOFqjpHa5VexUB5CxefvImptwoDeyVhtMuCkZa8kE3t32X62k1OLEvorBXm3fnKeg00AkLUySgC';
-    const onToken = (token) => {
-        console.log("stripe-button.js, line 8, token: ",token)
-        alert("Payment successful")
-    }
+    const onToken = token => {
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('successful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
     return(
         <StripeCheckout
             label="Check Out Oliver"
